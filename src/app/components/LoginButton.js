b07827a6f4a5876d70b6eba804214071dfb9350d
Ian Huang 黃彥鈞 <q21tm85818@gmail.com>
2024-01-25 22:08:26 +0800

    feat: Add Prisma adapter

--- src/app/components/LoginButton.js
+++ src/app/components/LoginButton.js
@@ -6,5 +6,6 @@ export function LoginButton() {
     const { data: session } = useSession();
     const isLogin = session && session.user;
     const callback = isLogin ? signOut : signIn;
-    return <button onClick={() => { callback(); }}>{callback.name}</button>
+    const text = isLogin ? 'logout' : 'login';
+    return <button onClick={() => { callback(); }}>{text}</button>
 }
--- src/app/components/Providers.js
+++ src/app/components/Providers.js
@@ -1,6 +1,6 @@
 "use client"
 
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react"
 
 export default function Providers({ children }) {
